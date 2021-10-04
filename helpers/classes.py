f78e84b2bb32260b7c3c7d355d9a14448e67f706
jackmitt <jjmittenthal@crimson.ua.edu>
2021-10-04 19:22:53 +0200

    2

---
+++ helpers/classes.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy and np
+
+class Database:
+    def __init__(self, keys):
+        self.df = pd.DataFrame()
+        self.dict = {}
+        for key in keys:
+            self.dict[key] = []
+        self.tempRow = []
+
+    def addCellToRow(self, datum):
+        if (len(self.tempRow) + 1 > len(self.dict)):
+            raise ValueError("The row is already full")
+        else:
+            self.tempRow.append(datum)
+
+    def appendRow(self):
+        if (len(self.tempRow) != len(self.dict)):
+            raise ValueError("The row is not fully populated")
+        else:
+            for i in range(len(self.dict.keys())):
+                self.dict[self.dict.keys()[i]].append(self.tempRow[i])
+            self.tempRow = []
+
+    def dictToCsv(self, pathName):
+        self.df = pd.DataFrame.from_dict(self.dict)
+        dfFinal = dfFinal.drop_duplicates()
+        dfFinal.to_csv(pathName, index = False)
